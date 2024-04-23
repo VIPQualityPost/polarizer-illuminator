410c5dc5c9d4c415d01e090a32a47b5f6b40a8eb
matei jordache <mateijordache@live.com>
2024-04-22 21:15:29 -0700

    clean up code

--- firmware/src/dma.cpp
+++ firmware/src/dma.cpp
@@ -2,16 +2,13 @@
 
 void init_dma(void)
 {
-  __HAL_RCC_DMAMUX1_CLK_ENABLE();
-  __HAL_RCC_DMA1_CLK_ENABLE();
-
-  // HAL_NVIC_SetPriority(DMA1_Channel5_IRQn, 0, 0);
-  // HAL_NVIC_EnableIRQ(DMA1_Channel5_IRQn);
+  HAL_NVIC_SetPriority(DMA1_Channel3_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Channel3_IRQn);
 }
 
-// extern "C" {
-// void DMA1_Channel5_IRQHandler(void)
-// {
-//   HAL_DMA_IRQHandler(&hdma_adc1);
-// }
-// }
+extern "C" {
+void DMA1_Channel3_IRQHandler(void)
+{
+  HAL_DMA_IRQHandler(&hdma_adc1);
+}
+}
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -11,6 +11,7 @@
 #include "stm32g4xx_hal_opamp.h"
 
 #include "RTTStream.h"
+#include "PeripheralPins.h"
 #include "USBPowerDelivery.h"
 
 #define USBD_MANUFACTURER_STRING "matei repair lab"
@@ -22,24 +23,49 @@ extern OPAMP_HandleTypeDef hopamp1;
 
 RTTStream rtt;
 
-void userButton();
-
 bool IlluminatorEnabled = true;
+bool togglePolarizer = true;
+
 volatile uint16_t adc_data[2] = {0};
 
+void userButton(void){
+  if(IlluminatorEnabled){
+    PowerSink.requestPower(5000, 2000);
+    DAC3->CR &= ~DAC_CR_CEN1;
+  }
+  else{
+    PowerSink.requestPower(15000, 2000);
+    DAC3->CR |= DAC_CR_CEN1;
+  }
+
+  IlluminatorEnabled = !IlluminatorEnabled;
+}
+
+void updateControls(void){
+  if(togglePolarizer)
+    DAC3->DHR12R1 = 0x000;
+  else
+    DAC3->DHR12R1 = analogRead(LEFT_POT) << 2;
+
+  TIM2->CCR1 = analogRead(RIGHT_POT) << 6;
+  togglePolarizer = !togglePolarizer;
+}
+
 void setup() {
-  pinMode(PB8, INPUT);
-  // pinMode(PA0, INPUT_ANALOG);
-  // pinMode(PA1, INPUT_ANALOG);
-  pinMode(PA5, OUTPUT);
-  pinMode(PA8, OUTPUT);
+  pinMode(USER_BUTTON, INPUT);
+  pinMode(LEFT_POT, INPUT_ANALOG);
+  pinMode(RIGHT_POT, INPUT_ANALOG);
+  pinMode(LED_PWM, OUTPUT);
+  pinMode(LCD_PWM, OUTPUT);
+  pinMode(LCD_OPA, OUTPUT);
 
   attachInterrupt(PB8, userButton, RISING);
 
   PowerSink.start();
   PowerSink.requestPower(15000);
 
-  init_dma();
+  __HAL_RCC_DMAMUX1_CLK_ENABLE();
+  __HAL_RCC_DMA1_CLK_ENABLE();
 
   gpio_config_adc(&hadc1);
   init_adc();
@@ -54,53 +80,21 @@ void setup() {
   HAL_OPAMP_Start(&hopamp1);
   HAL_DAC_Start(&hdac3, DAC_CHANNEL_1);
   
-  TIM_TypeDef* IlluminatorTimer = (TIM_TypeDef*)pinmap_peripheral(digitalPinToPinName(PA5), PinMap_PWM);
-  uint32_t IlluminatorChannel = STM_PIN_CHANNEL(pinmap_function(digitalPinToPinName(PA5), PinMap_PWM));
+  TIM_TypeDef* IlluminatorTimer = TIM2;
+  uint32_t IlluminatorChannel = STM_PIN_CHANNEL(pinmap_function(digitalPinToPinName(LED_PWM), PinMap_PWM));
   HardwareTimer* Illuminator = new HardwareTimer(IlluminatorTimer);
   Illuminator->setMode(IlluminatorChannel, TIMER_OUTPUT_COMPARE_PWM1, PA5);
   Illuminator->setOverflow(2000, HERTZ_FORMAT);
-  Illuminator->setCaptureCompare(IlluminatorChannel, 90, HERTZ_COMPARE_FORMAT);
 
-  // TIM2->CR1 |= 0b1 << TIM_CR1_URS_Pos;
-  // TIM2->CR2 |= 0b010 << TIM_CR2_MMS_Pos;
-
-  TIM_TypeDef *PolarizerTimer = (TIM_TypeDef*)pinmap_peripheral(digitalPinToPinName(PA8), PinMap_PWM);
-  uint32_t PolarizerChannel = STM_PIN_CHANNEL(pinmap_function(digitalPinToPinName(PA8), PinMap_PWM));
+  TIM_TypeDef *PolarizerTimer = TIM1;
   HardwareTimer* Polarizer = new HardwareTimer(PolarizerTimer);
-  Polarizer->setMode(PolarizerChannel, TIMER_OUTPUT_COMPARE_PWM1, PA8);
+  Polarizer->attachInterrupt(updateControls);
   Polarizer->setOverflow(1000, HERTZ_FORMAT);
-  Polarizer->setCaptureCompare(PolarizerChannel, 50, PERCENT_COMPARE_FORMAT);
 
   Illuminator->resume();
   Polarizer->resume();
 }
 
 void loop(){ 
-  // DAC3->DHR12R1 = analogRead(PA0) << 2;
-  // TIM2->CCR1 = analogRead(PA1) << 6;
-
-  DAC3->DHR12R1 = adc_data[0] << 2;
-  TIM2->CCR1 = adc_data[1] << 6;
-
-  PowerSink.poll();
 }
 
-void userButton(void){
-  if(IlluminatorEnabled){
-    // Reduce losses from LDO
-    PowerSink.requestPower(5000, 2000);
-    IlluminatorEnabled = false;
-    // Turn off peripherals, saves about 100mW idle
-    TIM1->CR1 &= ~TIM_CR1_CEN;
-    TIM2->CR1 &= ~TIM_CR1_CEN;
-    DAC3->CR &= ~DAC_CR_CEN1;
-  }
-  else{
-    PowerSink.requestPower(15000, 2000);
-    IlluminatorEnabled = true;
-    // Turn on peripherals
-    TIM1->CR1 |= TIM_CR1_CEN;
-    TIM2->CR1 |= TIM_CR1_CEN;
-    DAC3->CR |= DAC_CR_CEN1;
-  }
-}
